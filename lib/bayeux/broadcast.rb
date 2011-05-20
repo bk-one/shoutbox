f533d0858dc053262c8667ba8130af92d7136486
Benjamin Krause <bk@benjaminkrause.com>
2011-05-20 15:59:33 +0200

    probably fixed shout bug

--- lib/bayeux/broadcast.rb
+++ lib/bayeux/broadcast.rb
@@ -6,13 +6,18 @@ class Shoutbox
 
       def self.message( account_name, auth_token, message )
         EM.run {
-          client.add_extension(Shoutbox::Bayeux::ClientAuth.new(auth_token))
           client.publish('/status/' + account_name, message)
         }
       end
 
       def self.client
-        @client ||= Faye::Client.new(Shoutbox.full_host + "/bayeux")
+        @client ||= create_client
+      end
+      
+      def self.create_client  
+        client = Faye::Client.new(Shoutbox.full_host + "/bayeux")
+        client.add_extension(Shoutbox::Bayeux::ClientAuth.new(auth_token))
+        client
       end
     end
   end
